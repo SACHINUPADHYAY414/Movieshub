ad9cf4835e511c901a1f46bdd2c3730d05b84d6e
Sachin Kumar Upadhyay <137228483+SACHINUPADHYAY414@users.noreply.github.com>
2024-05-12 16:44:12 +0530

    Update apiFavorites.js

--- client/src/api/apiFavorites.js
+++ client/src/api/apiFavorites.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const favorite = axios.create({
-  baseURL: "http://localhost:8000/",
+  baseURL: "https://movieshub-hy9l.onrender.com",
 });
 
 export default favorite;
